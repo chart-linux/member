f0d4b4d0914c1cf1a411b1e4481a0e505e0f8aa8
Y.Sangenya <longinus.eva@gmail.com>
2016-03-19 10:23:22 +0900

    ナビゲーションに、所属から構成員を絞る項目を追加した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_menu
+
+  private
+
+  def set_menu
+    @menu_rooms = Room.all
+    @menu_departments = Department.all
+    @menu_committees = Committee.all
+  end
 end
