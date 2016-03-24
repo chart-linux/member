e76add081209c779600ae1236c71180689a1c1fa
Y.Sangenya <longinus.eva@gmail.com>
2016-03-25 01:30:41 +0900

    memberモデルを作成/変更/削除したときに、メーリスに加入/メーリスから削除するようにした

--- app/models/member.rb
+++ app/models/member.rb
@@ -7,4 +7,16 @@ class Member < ApplicationRecord
   validates :name, presence: true
 
   default_scope { order :room_id, :department_id, :committee_id }
+
+  before_save do
+    MailingListMailer.add_address(mail_address).deliver
+  end
+
+  before_update do
+    MailingListMailer.bye_address(mail_address_was).deliver if mail_address_changed?
+  end
+
+  before_destroy do
+    MailingListMailer.bye_address(mail_address).deliver
+  end
 end
